a68a2511a1aa4746ae50ea29a5b4bab819f03368
LEE-NS <ogawa9341@gmail.com>
2024-06-01 22:03:21 +0900

    update : some logics

--- src/components/NewsfeedCreate.jsx
+++ src/components/NewsfeedCreate.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import CloseIcon from "@mui/icons-material/Close";
 import SendIcon from "@mui/icons-material/Send";
