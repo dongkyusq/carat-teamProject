630bf482b0fef1c1fc04dc2e5d75e70c150a3588
LEE-NS <ogawa9341@gmail.com>
2024-06-03 17:25:51 +0900

    Update : env file

---
+++ src/components/Newsfeed.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Newsfeed() {
+  return (
+    <div>
+      <h2>작성된 뉴스피드가 들어오는 공간이다.</h2>
+      <div>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default Newsfeed;
