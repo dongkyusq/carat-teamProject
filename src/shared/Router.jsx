a1a367d1bffe98960910f22ccddea909e18b9ead
LEE-NS <ogawa9341@gmail.com>
2024-06-05 22:23:09 +0900

    merge with my branch

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "../components/Layout";
-import Login from "../components/Login";
 import Join from "../components/Join";
 import Home from "../pages/Home";
 import Post from "../pages/Post";
