ce7d32a20864fd0b32b76ed788b7214260f2aab5
SNGCHN <sngchn0312@naver.com>
2024-06-05 19:33:08 +0900

    merge

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,9 +1,9 @@
-import React, { useRef, useState } from 'react'
-import Modal from '../pages/Modal';
-import { styled } from 'styled-components';
-import { v4 as uuidv4 } from 'uuid'; //yarn add uuid
-import supabase from '../supabaseClient';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef, useState } from "react";
+import Modal from "../pages/Modal";
+import { styled } from "styled-components";
+import { v4 as uuidv4 } from "uuid"; //yarn add uuid
+import supabase from "../supabaseClient";
+import { useNavigate } from "react-router-dom";
 
 const Join = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,7 +15,7 @@ const Join = () => {
   const confirmpwRef = useRef();
   const mbtiRef = useRef();
 
-  const signUpUser = async (e) => {
+  const signUpUser = async e => {
     e.preventDefault();
     const { data, error } = await supabase.auth.signUp({
       name: nameRef.current.value,
@@ -30,12 +30,12 @@ const Join = () => {
     if (error) {
       console.log(error);
     } else {
-      console.log('authdata:', data);
+      console.log("authdata:", data);
     }
 
     const user = data.user;
     if (user) {
-      const { error: insertError } = await supabase.from('user_data').insert([
+      const { error: insertError } = await supabase.from("user_data").insert([
         {
           name: nameRef.current.value,
           nickname: nicknameRef.current.value,
@@ -44,27 +44,25 @@ const Join = () => {
           confirmpassword: confirmpwRef.current.value,
           id: uuidv4(),
           mbti: mbtiRef.current.value,
-        }
+        },
       ]);
       if (insertError) {
         console.log(insertError);
       } else {
-        console.log('insertdata:', data);
-        alert('회원가입이 완료되었습니다');
+        console.log("insertdata:", data);
+        alert("회원가입이 완료되었습니다");
         setIsModalOpen(false);
-        navigate('/login');
-
+        navigate("/login");
       }
     }
   };
 
   const openModal = () => {
     setIsModalOpen(true);
-  }
+  };
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
 
   return (
     <div>
@@ -84,12 +82,12 @@ const Join = () => {
             </JoinInput>
 
             <JoinInput>
-              <Lable> 아이디 * :  </Lable>
+              <Lable> 아이디 * : </Lable>
               <Input type="email" placeholder="이메일 형식으로 입력해주세요" ref={idRef} required />
             </JoinInput>
 
             <JoinInput>
-              <Lable> 비밀번호 * :  </Lable>
+              <Lable> 비밀번호 * : </Lable>
               <Input type="password" placeholder="비밀번호를 입력해주세요" ref={pwRef} required />
             </JoinInput>
 
@@ -99,23 +97,25 @@ const Join = () => {
             </JoinInput>
 
             <JoinInput>
-              <Lable> MBTI * :  </Lable>
+              <Lable> MBTI * : </Lable>
               <Input type="text" placeholder="대문자로 입력해주세요" ref={mbtiRef} required />
             </JoinInput>
 
             <Text> *는 필수 </Text>
-            <Button type="submit" onClick={signUpUser}>회원가입</Button>
+            <Button type="submit" onClick={signUpUser}>
+              회원가입
+            </Button>
           </Form>
         </JoinWrapper>
       </Modal>
     </div>
-  )
+  );
 };
 
 export default Join;
 
 const Button = styled.button`
-  background-color: #FFD0D0;
+  background-color: #ffd0d0;
   color: black;
   border: none;
   padding: 10px 80px;
@@ -129,11 +129,11 @@ const Button = styled.button`
   }
 `;
 const JoinWrapper = styled.div`
-display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #CC8798;
+  background-color: #cc8798;
   padding: 20px;
   border-radius: 15px;
   width: 850px;
@@ -145,11 +145,11 @@ display: flex;
 const Circle = styled.div`
   width: 50px;
   height: 50px;
-  background-color: #FFD0D0;
+  background-color: #ffd0d0;
   border-radius: 50%;
   position: absolute;
   top: -25px;
-  left: -25px; 
+  left: -25px;
   z-index: 1;
 `;
 
@@ -163,7 +163,6 @@ const Form = styled.form`
   width: 750px;
   height: 7500px;
   justify-content: center;
-    
 `;
 
 const JoinInput = styled.div`
@@ -193,5 +192,3 @@ const Text = styled.div`
   font-size: 15px;
   color: red;
 `;
-
-
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import Layout from "../components/Layout";
 import Login from "../components/Login";
-=======
->>>>>>> 25b5930d34c6107ae13589ea3e77c98e9e3a9d09
 import Join from "../components/Join";
 import Home from "../pages/Home";
 import Post from "../pages/Post";
@@ -18,10 +15,7 @@ const Router = () => {
         <Route path="/post" element={<Post />} />
         {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/join" element={<Join />} />
-<<<<<<< HEAD
         <Route path="/mypage" element={<MyPage />} />
-=======
->>>>>>> 25b5930d34c6107ae13589ea3e77c98e9e3a9d09
       </Routes>
     </BrowserRouter>
   );
